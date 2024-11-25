19b470d45a6b6d5cdea29ef6553dad794b495402
marcosarias21 <marcosariasolaiz@gmail.com>
2024-11-25 14:01:49 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Ejemplo: Personaliza tus reglas aquí
+    'react/react-in-jsx-scope': 'off', // No es necesario importar React en proyectos modernos
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+};
