c0821cb5e90f3dc0177d2944a28836502c35f4c0
marcosarias21 <marcosariasolaiz@gmail.com>
2024-12-16 20:09:54 -0300

    added cards with data provided by api ML

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,25 @@
 import { useEffect } from 'react'
 import { Navbar } from '../../components/Navbar'
-import useFetch from '../../hooks/useFetch'
-import { Categories } from '../../types/types.d'
-import { useCategoriesStore } from '../../components/store/categoriesStore'
+import { CategorieCard } from '../../components/CategorieCard'
 import { Carousel } from '../../components/Carousel'
+import { CardProducts } from '../../components/CardProducts'
+import useFetch from '../../hooks/useFetch'
 import useScroll from '../../hooks/useScroll'
-import { Container, Typography } from '@mui/material'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules'
+import { useCategoriesStore } from '../../store/categoriesStore'
+import { ArrayProductsDiscount, Categories } from '../../types/types.d'
+import { Box, Container, Grid2, Typography } from '@mui/material'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Home = () => {
     const categoriesSetter = useCategoriesStore((state) => state.setCategories)
     const categories = useFetch<Categories>('https://api.mercadolibre.com/sites/MLA/categories')
+    const discountProducts = useFetch<ArrayProductsDiscount>('https://api.mercadolibre.com/sites/MLA/search?q=ipod&FilterID=discount&limit=12')
+    const categoryIds = ['MLA1051', 'MLA1648', 'MLA1574']
+    console.log(discountProducts)
     const { isShow } = useScroll()
 
     useEffect(() => {
@@ -20,9 +30,36 @@ const Home = () => {
     <>
       <Navbar isShow={isShow} />
       <Carousel />
-      <Container maxWidth={'lg'} sx={{ marginTop: '50px' }}>
-        <Typography component={'h4'} variant='h4' fontWeight={'bold'}>Destacados</Typography>
-      </Container>
+      <Box sx={{ marginTop: '30px', backgroundColor: '#f8f9fa', paddingTop: 4}}>
+        <Container maxWidth={'lg'} >
+          <Typography component={'h4'} variant='h4' fontWeight={'bold'}>Destacados</Typography>
+          <Box marginTop={5}>
+            <Swiper 
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={10}
+              slidesPerView={5}
+              navigation
+              scrollbar={{ draggable: true }}
+              loop={true}>
+              {
+                discountProducts?.data?.results.map(product => 
+                <SwiperSlide key={product.id} >
+                  <CardProducts {...product} />
+                </SwiperSlide>
+              )
+              }        
+            </Swiper>
+          </Box>
+          <Box mt={10}>
+            <Typography mb={2} component={'h5'} variant='h4' textAlign={'center'} fontWeight={'bold'}>Mas categorias</Typography>
+            <Grid2 container spacing={2}>
+                {categoryIds.map(id => <CategorieCard idCategory={id} key={id} />)}
+            </Grid2>
+          </Box>
+        </Container>
+        <footer>@Marcos Arias 2024</footer>
+      </Box>
+       
     </>
   )
 }
--- src/types/types.d.tsx
+++ src/types/types.d.tsx
@@ -1,21 +1,162 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export interface Root {
+export interface ArrayProductsDiscount {
+ results: ProductsDiscount[]
+}
+export interface ProductsDiscount {
   id: string
-  name: string
-  picture: string
+  title: string
+  condition: string
+  thumbnail_id: string
+  catalog_product_id?: string
+  listing_type_id: string
+  sanitized_title: string
   permalink: string
-  total_items_in_this_category: number
-  path_from_root: PathFromRoot[]
-  children_categories: ChildrenCategory[]
-  attribute_types: string
-  settings: Settings
-  channels_settings: ChannelsSetting[]
-  meta_categ_id: any
-  attributable: boolean
-  date_created: string
+  buying_mode: string
+  site_id: string
+  category_id: string
+  domain_id: string
+  thumbnail: string
+  currency_id: string
+  order_backend: number
+  price: number
+  original_price?: number
+  sale_price: SalePrice
+  available_quantity: number
+  official_store_id?: number
+  official_store_name?: string
+  use_thumbnail_id: boolean
+  accepts_mercadopago: boolean
+  shipping: Shipping
+  stop_time: string
+  seller: Seller
+  address: Address
+  attributes: Attribute[]
+  installments?: Installments
+  winner_item_id: any
+  catalog_listing: boolean
+  discounts: any
+  promotion_decorations: any
+  promotions: any
+  inventory_id?: string
 }
 
-export interface Data {
+export interface SalePrice {
+  price_id: string
+  amount: number
+  conditions: Conditions
+  currency_id: string
+  exchange_rate: any
+  payment_method_prices: any[]
+  payment_method_type: string
+  regular_amount?: number
+  type: string
+  metadata: Metadata
+}
+
+export interface Conditions {
+  eligible: boolean
+  context_restrictions: string[]
+  start_time?: string
+  end_time?: string
+}
+
+export interface Metadata {
+  promotion_id?: string
+  promotion_type?: string
+  campaign_id?: string
+  funding_mode?: string
+  order_item_price?: number
+  campaign_end_date?: string
+  discount_meli_amount?: number
+  experiment_id?: string
+  variation?: string
+  campaign_discount_percentage?: number
+}
+
+export interface Shipping {
+  store_pick_up: boolean
+  free_shipping: boolean
+  logistic_type: string
+  mode: string
+  tags: string[]
+  benefits: any
+  promise: any
+  shipping_score: number
+}
+
+export interface Seller {
+  id: number
+  nickname: string
+}
+
+export interface Address {
+  state_id: string
+  state_name: string
+  city_id?: string
+  city_name: string
+}
+
+export interface Attribute {
+  id: string
+  name: string
+  value_id?: string
+  value_name: string
+  attribute_group_id: string
+  attribute_group_name: string
+  value_struct?: ValueStruct
+  values: Value[]
+  source: number
+  value_type: string
+}
+
+export interface ValueStruct {
+  number: number
+  unit: string
+}
+
+export interface Value {
+  id?: string
+  name: string
+  struct?: Struct
+  source: number
+}
+
+export interface Struct {
+  number: number
+  unit: string
+}
+
+export interface Installments {
+  quantity: number
+  amount: number
+  rate: number
+  currency_id: string
+  metadata: Metadata2
+}
+
+export interface Metadata2 {
+  meliplus_installments: boolean
+  additional_bank_interest: boolean
+}
+
+
+export interface IconNavItem {
+  icon: JSX.Element
+  textInfo: string
+  button: boolean
+}  
+
+export type Categories = Categorie[]
+
+export interface Categorie {
+  id: string
+  name: string
+}
+
+
+
+
+export interface Categorie {
   id: string
   name: string
   picture: string
@@ -62,7 +203,7 @@ export interface Settings {
   max_variations_allowed: number
   maximum_price: any
   maximum_price_currency: string
-  minimum_price: any
+  minimum_price: number
   minimum_price_currency: string
   mirror_category: any
   mirror_master_category: any
@@ -82,7 +223,7 @@ export interface Settings {
   tags: any[]
   vertical: string
   vip_subdomain: string
-  buyer_protection_programs: any[]
+  buyer_protection_programs: string[]
   status: string
 }
 
@@ -99,15 +240,6 @@ export interface Settings2 {
 }
 
 
-export interface IconNavItem {
-  icon: JSX.Element
-  textInfo: string
-  button: boolean
-}  
 
-export type Categories = Categorie[]
 
-export interface Categorie {
-  id: string
-  name: string
-}
+
